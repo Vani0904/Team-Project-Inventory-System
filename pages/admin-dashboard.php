4da0bd37604d80ee2789287de19280b66ca95f3d
Vani0904 <C3009616@hallam.shu.ac.uk>
2024-11-06 17:33:01 +0000

    updating company logo size for admin dashboard

--- pages/admin-dashboard.php
+++ pages/admin-dashboard.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../style/desktop.css"> 
+    <link rel="stylesheet" href="../styles/desktop.css"> 
 </head>
 <body>
     <div class="admin-db-container">
