e99c37456073d351f596b78a0e1567ff69655de2
Vani0904 <C3009616@hallam.shu.ac.uk>
2024-11-16 14:16:23 +0000

    Fixed merging issue & Implementing inventory table

--- includes/header-admin.php
+++ includes/header-admin.php
@@ -1,4 +1,4 @@
-<header class="header-admin">
+<section class="header-admin">
     <label for="menu-toggle" class="hamburger-icon">
         <img src="../images/admin-dashboard-menu-icon-black.png" alt="admin dashboard icon">
     </label>
@@ -6,4 +6,4 @@
     <label class="header-admin-details">[Last Name]</label>
     <img class="header-admin-icon" src="" width="50" height="50">
     <img class="header-admin-icon" src="../images/log-out-icon.png" width="50" height="50">
-</header>
+</section>
--- pages/admin-dashboard.php
+++ pages/admin-dashboard.php
@@ -1,14 +1,13 @@
 <?php //include '../includes/check-if-admin.php'; ?> <!--This will be required once we have database integration.-->
+<?php include "../includes/connectdb.php";?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="../styles/desktop.css">
 </head>
-
 <body>
     <div class="admin-db-container">
         <input type="checkbox" id="menu-toggle" class="menu-toggle">
@@ -26,6 +25,8 @@
                     <p>£7,500</p>
                 </div>
                 <div>
+                    <?php
+                    ?>
                     <h2>Rate Of Return</h2>
                     <p>7%</p>
                 </div>
@@ -45,7 +46,6 @@
                     <select id="category" name="category">
                         <option value="Perfume">Perfume</option>
                         <option value="deodorant">Deodorant </option>
-                        <option value="Saint Laurent">Saint Laurent</option>
                     </select>
                 </div>
                 <div id="search-inventory-product">
@@ -67,35 +67,38 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td data-label="ID">1</td>
-                            <td data-label="Name">Perfume</td>
-                            <td data-label="Branch">Sheffield</td>
-                            <td data-label="Price">£49.99</td>
-                            <td data-label="Quantity">8</td>
-                            <td data-label="Category">Dior</td>
-                            <td data-label="Actions">
-                                <a href="" class="edit-item-btn">Edit</a>
-                                <a href="" class="delete-item-btn"
-                                    onclick="return confirm('Are you sure you want to delete this Product?')">Delete</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td data-label="ID">2</td>
-                            <td data-label="Name">Deodrant</td>
-                            <td data-label="Branch">Leeds</td>
-                            <td data-label="Price">£19.99</td>
-                            <td data-label="Quantity">5</td>
-                            <td data-label="Category">Dior</td>
-                            <td data-label="Actions">
-                                <a href="" class="edit-item-btn">Edit</a>
-                                <a href="" class="delete-item-btn"
-                                    onclick="return confirm('Are you sure you want to delete this Product?')">Delete</a>
-                            </td>
-                        </tr>
-                        <!-- <tr>
-                        <td colspan="7"> No Record Found</td>
-                    </tr> -->
+                        <?php
+                            $query = "SELECT * FROM product";
+                            $result = mysqli_query($connection,$query);
+
+                            if (mysqli_num_rows($result) > 0)
+                            {
+                                while ($productColumn = mysqli_fetch_assoc($result))
+                                {
+                                    ?>
+                                    <tr>
+                                        <td data-label="ID"><?php echo $productColumn['product_id']; ?></td>
+                                        <td data-label="Name"><?php echo $productColumn['name']; ?></td>
+                                        <td data-label="Branch"><?php echo $productColumn['branch_id']; ?></td>
+                                        <td data-label="Price">£<?php echo $productColumn['unit_price']; ?></td>
+                                        <td data-label="Quantity"><?php echo $productColumn['quantity']; ?></td>
+                                        <td data-label="Category"><?php echo $productColumn['category']; ?></td>
+                                        <td data-label="Actions">
+                                        <a href="" class="edit-item-btn">Edit</a>
+                                        <a href="" class="delete-item-btn"
+                                            onclick="return confirm('Are you sure you want to delete this Product?')">Delete</a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            } else{
+                                ?>
+                                <tr>
+                                    <td colspan="7"> No Record Found</td>
+                                </tr>
+                                <?php
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
