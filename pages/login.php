c86f5f7485da2f88c99f5077d1ddd3bfdff472bf
Khalifa Mussa <Khalifa.mussa12@outlook.com>
2024-11-20 12:10:43 +0000

    register - sign up (title)

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" media="only screen and (min-device-width: 737px)" href="../styles/mobile.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Elegance Scent</title>
+    <title>Login</title>
 </head>
 <body>
     <img src="images" alt="">
