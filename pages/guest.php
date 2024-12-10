abd21a994e0bb900bd744fb9adda0bb697161c8f
Elekiir <149782257+Elekir@users.noreply.github.com>
2024-12-10 00:50:32 +0000

    added script

--- pages/guest.php
+++ pages/guest.php
@@ -185,30 +185,7 @@
     <script src="../scripts/jquery-3.7.1.js" defer></script>
     <script src="../scripts/footer.js" defer></script>
     <script type="text/javascript" src="../scripts/darkmode.js" defer></script>
-<!-------------script- for- filtering active link-------->
-<script>
-  var selector = '.our-p-products li';
+    <script src="../scripts/guest-filtering.js" defer></script>
 
-  $(selector).on('click', function(){
-      $(selector).removeClass('active')
-      $(this).addClass('active')
-  });
-
-  /*---box filter */
-  $(document).ready(function (){
-    $('.our-p-products li').click(function(){
-      const value = $(this).attr('data-filter');
-      if(value == 'mix'){
-        $('.our-product-box').show('1000');
-      }
-      else{
-        $('.our-product-box').not('.'+value).hide('1000');
-        $('.our-product-box').filter('.'+value).show('1000');
-      }
-    });
-
-  });
-</script>
-  
   </body>
 </html>
